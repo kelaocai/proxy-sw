cdb1bea24294ccf24bbbf31acebe2d3c9f198094
kelaocai <kelaocai@163.com>
2026-03-09 17:40:31 +0800

    chore: update homebrew formula for v0.1.1

--- packaging/homebrew/proxy-sw.rb
+++ packaging/homebrew/proxy-sw.rb
@@ -1,18 +1,18 @@
 class ProxySw < Formula
   desc "macOS system proxy manager for local proxy tools like Clash"
   homepage "https://github.com/kelaocai/proxy-sw"
-  version "0.0.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/kelaocai/proxy-sw/releases/download/v0.0.0/proxy-sw_0.0.0_macos_arm64.tar.gz"
-      sha256 "REPLACE_ARM64_SHA256"
+      url "https://github.com/kelaocai/proxy-sw/releases/download/v0.1.1/proxy-sw_0.1.1_macos_arm64.tar.gz"
+      sha256 "395592cb89ef5d5147dd65ff6e2d6a58bd930460705c056f49780a3276567543"
     end
 
     on_intel do
-      url "https://github.com/kelaocai/proxy-sw/releases/download/v0.0.0/proxy-sw_0.0.0_macos_x86_64.tar.gz"
-      sha256 "REPLACE_AMD64_SHA256"
+      url "https://github.com/kelaocai/proxy-sw/releases/download/v0.1.1/proxy-sw_0.1.1_macos_x86_64.tar.gz"
+      sha256 "7518479c0d70ea11152b8b80ce99b5da977a108cff3a3c12401360a70300b9ef"
     end
   end
 
